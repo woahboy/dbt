af182e6c43097dfbd1e984cef37c2ba5e4cc2153
justintime94@gmail.com <justintime94@gmail.com>
2025-08-24 04:21:50 +0000

    incremental

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,6 +1,7 @@
-{{
-    config(
-        materialized='view'
+{{ config(
+        materialized='incremental',        
+        unique_key="order_id",
+        incremental_strategy="merge"
     )
 }}
 
@@ -9,8 +10,9 @@ with orders as (
 ),
 
 payments as (
-    select * from {{ ref('stg_payments')}}
+    select * from {{ ref('stg_stripe__payments')}}
 ),
+
 
 order_payments as (
     select 
@@ -28,8 +30,14 @@ final as (
         orders.customer_id,
         orders.order_date,
         coalesce (order_payments.amount, 0) as amount
+
     from orders
     left join order_payments using (order_id)
 )
 
 select * from final
+
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where order_date > (select max(order_date) from {{ this }}) 
+{% endif %}
